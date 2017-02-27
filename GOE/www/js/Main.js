a8e02764b781e43afb350e19face1d224c85be4b
Danni Rosing <rosingdanni@gmail.com>
2017-02-27 09:07:59 +0100

    Setup

---
+++ GOE/www/js/Main.js
@@ -0,0 +1,30 @@
+﻿///Forside menu
+$(function () {
+
+    $myMenu = $("#myMenu");
+    $.ajax({
+        type: 'GET',
+        url: 'http://localhost:13105/api/Get/GetMenu',
+        success: function (posts) {
+            $.each(posts, function (i, post) {
+                $myMenu.append("<li class='menupunkter'><a href=\"" + post.Navn + ".html" + "\".>" + post.Navn + " </a></li>");
+            });
+        }
+    });
+
+});
+$(function () {
+
+    $myforum = $("#mydiv");
+    $.ajax({
+        type: 'GET',
+        url: 'http://localhost:13105/api/Get/GetForum',
+        success: function (posts) {
+            $.each(posts, function (i, post) {
+                $myforum.append("<div class='foverskrift'>" + post.Overskrift + "</div> ");
+
+            });
+        }
+    });
+
+});
